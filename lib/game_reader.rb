6f353ffd269c4c24beb2d73a63c0c019525bf679
lilwillifo <margaretwilliford4@gmail.com>
2018-02-07 10:40:50 -0700

    initial commit game reader

--- lib/game_reader.rb
+++ lib/game_reader.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class GameReader
+  def initialize(file)
+    @file = file
+  end
 
+  def read_file
+    file = File.readlines(@file)
+    file.map do |line|
+      line.chomp.split(',')
+    end
+  end
+
+  def word_count(player)
+  end
 end

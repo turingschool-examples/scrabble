66627271dc1509297b70b52af4bce95b62f4fd56
alexcutschall <alexander.cutschall@gmail.com>
2018-02-07 09:07:31 -0700

    Added the letters hash

--- test/game_reader_test.rb
+++ test/game_reader_test.rb
@@ -6,4 +6,21 @@ require 'pry'
 
 class GameReaderTest < Minitest::Test
 
+  def test_game_reader_is_instantiated
+    game = GameReader.new
+    assert_instance_of GameReader, game
+  end
+
+  def test_game_reader_has_letter_has
+    game = GameReader.new
+    assert_equal ({
+      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+      "Y"=>4, "Z"=>10
+      }), game.letters
+  end
 end

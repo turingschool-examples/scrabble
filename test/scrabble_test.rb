79cad51f2defeda26e0b0bec913045ea5ca7f197
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-07 09:36:24 -0700

    Added nil test

--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -13,13 +13,14 @@ class ScrabbleTest < Minitest::Test
     assert_equal 1, @game.score('a')
     assert_equal 4, @game.score('f')
     assert_equal 0, @game.score('')
+    assert_equal 0, @game.score(nil)
   end
 
   def test_it_can_score_a_wordr
     assert_equal 8, @game.score('hello')
   end
 
-  def test_it_can_do_multipliers                      
+  def test_it_can_do_multipliers
     bonus10 = @game.score_with_multipliers('sparkle', [1, 2, 1, 3, 1, 2, 1], 2)
 
     assert_equal 9,  @game.score_with_multipliers('hello', [1, 2, 1, 1, 1])

c337afe2b28fbf9d230057ee65b6b47fa0f72c88
lilwillifo <margaretwilliford4@gmail.com>
2018-02-07 09:19:25 -0700

    initial commit

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,18 +1,23 @@
 class Scrabble
 
-  def score(word)
-    1
+  def initialize
+    @point_values = {
+                      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+                      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+                      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+                      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+                      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+                      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+                      "Y"=>4, "Z"=>10
+                    }
   end
 
-  def point_values
-    {
-      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
-      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
-    }
+  def score(word)
+    letters = word.upcase.chars
+    score = 0
+    letters.each do |letter|
+      score += @point_values[letter]
+    end
+    score
   end
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -6,8 +6,11 @@ require 'pry'
 
 class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
-    skip
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
+  end
+
+  def test_it_can_score_two_letter_word
+    assert_equal 2, Scrabble.new.score('at')
   end
 end

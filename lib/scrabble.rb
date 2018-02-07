b86f4a7a7a9a194f04fe47006eca802f10192133
JerrelMitchell <jerrelmitchell@ymail.com>
2018-02-07 10:53:49 -0700

    work on score_withmultipliers method

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,5 +1,4 @@
 class Scrabble
-
 attr_reader :word
 
   def initialize
@@ -7,7 +6,7 @@ attr_reader :word
   end
 
   def score(word)
-    if word == nil
+    if word.nil?
       points = 0
     else
       chars = word.upcase.split('')
@@ -21,29 +20,23 @@ attr_reader :word
   end
 
   def score_with_multipliers(word)
-    if word == nil
+    if word.nil?
       points = 0
     else
-      chars = word.upcase.split('')
 
-      points = 0
-      chars.each do |char|
-      points += point_values[char]
-      end
+
+
     end
     points
-
   end
 
   def point_values
-    {
-      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+    { "A"=>1, "B"=>3, "C"=>3, "D"=>2,
       "E"=>1, "F"=>4, "G"=>2, "H"=>4,
       "I"=>1, "J"=>8, "K"=>5, "L"=>1,
       "M"=>3, "N"=>1, "O"=>1, "P"=>3,
       "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
       "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
-    }
+      "Y"=>4, "Z"=>10 }
   end
 end

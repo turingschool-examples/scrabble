397c5b3e217194d2ce395df11b79a7ff4ca71895
Sabrina Robinson <sabrinarob@gmail.com>
2018-02-07 18:38:59 -0700

    Completed scrabble with mentor.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,26 +1,42 @@
 class Scrabble
 require 'pry'
+def point_values
+  {
+    "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+    "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+    "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+    "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+    "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+    "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+    "Y"=>4, "Z"=>10
+  }
+end
 
-  def score(score)
+  def score(word)
+    return 0 if word.nil?
 
-     total = 0
-     point_values.each_key do |letter|
-       letter = point_values.keys + point_values.values
-       total += 1
-     end
-     total
+    word.upcase.chars.map do |letter|
+      point_values[letter]
+    end.sum + seven_letter_bonus(word)
+
   end
 
-  def point_values
-    {
-      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
-      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
-    }
+  def score_with_multipliers(word, letter_multiplier, word_multiplier = 1)
+    return 0 if word.nil?
+
+    word_sum = word.upcase.chars.each_with_index.map do |letter, index|
+      point_values[letter] * letter_multiplier[index]
+    end.sum * word_multiplier
+
+    word_sum + seven_letter_bonus(word)
   end
 
+  def seven_letter_bonus(word)
+
+    if word.length == 7
+       50
+    else
+      0
+    end
+  end
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -2,6 +2,7 @@ gem 'minitest'
 require_relative '../lib/scrabble'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'rake'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
@@ -10,5 +11,37 @@ class ScrabbleTest < Minitest::Test
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
 
+  end
+
+  def test_it_can_score_a_word
+
+    assert_equal 8, Scrabble.new.score("hello")
+  end
+
+  def test_it_can_score_nil
+    assert_equal 0, Scrabble.new.score(nil)
+  end
+
+  def test_it_can_score_with_letter_multipilers
+
+    result = Scrabble.new.score_with_multipliers("hello", [1,3,1,1,1])
+    assert_equal 10, result
+  end
+
+  def test_it_can_score_with_word_multipilers
+
+    result = Scrabble.new.score_with_multipliers("hello", [1,3,1,1,1], 2)
+    assert_equal 20, result
+  end
+
+  def test_it_can_score_a_seven_letter_bonus
+
+    result = Scrabble.new.score("morning")
+    assert_equal 60, result
+  end
+
+  def test_it_can_score_a_seven_letter_bonus_with_multipliers
+    result = Scrabble.new.score_with_multipliers("morning", [1,1,1,1,1,1,1])
+    assert_equal 60, result
   end
 end

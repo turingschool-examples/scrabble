ef95dc9397494db466b06f0297f15b9ad9e13070
mellisaluce <mjlgluce@gmail.com>
2018-05-16 10:24:42 -0600

    adds score word

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,18 +1,27 @@
+require 'pry'
 class Scrabble
+  attr_reader :point_values
 
-  def score(word)
-    1
-  end
-
-  def point_values
-    {
-      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
-      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
+  def initialize
+    @point_values = {
+    "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+    "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+    "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+    "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+    "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+    "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+    "Y"=>4, "Z"=>10
     }
   end
+
+  def score(word)
+    word.upcase.chars.map do |letter|
+      @point_values[letter]
+    end.sum
+  end
 end
+#
+# k = Scrabble.new
+# p k.score("hello")
+# p k.score("a")
+# p k.score("w")

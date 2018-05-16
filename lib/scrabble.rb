6ad8ce5d0a822eb0c7e245aed91fd7b180b4e64c
Pat McCartney <patmccartney@Pats-MacBook-Pro.local>
2018-05-16 11:08:18 -0600

    showed up a couple minutes late worked on the exercises out in the hallway.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,18 +1,48 @@
 class Scrabble
 
-  def score(word)
-    1
+  def score(guess_word)
+    guess_word_split = take_word(guess_word)
+    total_score = assign_point_values(guess_word_split)
+    p total_score
   end
 
-  def point_values
-    {
-      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
-      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
-    }
+
+  def take_word(word)
+    if word == " "
+      p 0
+    elsif word == '0'
+      p 0
+    elsif
+    string = word.to_s
+    array = string.chars
+    end
   end
+
+    def assign_point_values(split_word)
+      point_values =     {
+            "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+            "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+            "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+            "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+            "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+            "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+            "Y"=>4, "Z"=>10
+          }
+
+          score = []
+          value_0 = point_values[split_word[0]]
+          value_1 = point_values[split_word[1]]
+          value_2 = point_values[split_word[2]]
+          value_3 = point_values[split_word[3]]
+          value_4 = point_values[split_word[4]]
+          score << value_0
+          score << value_1
+          score << value_2
+          score << value_3
+          score << value_4
+          word_total_score = score.sum
+    end
+
+
 end
+Scrabble.new.score(" ")
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -6,8 +6,23 @@ require 'pry'
 
 class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
-    skip
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end
-end
+
+  def test_it_recieves_word
+    word_array = Scrabble.new.score("hello")
+    assert_equal "h", word_array[0]
+  end
+
+  def test_can_recognize_nill
+    guess_word = Scrabble.new.take_word(" ")
+    assert_equal 0, guess_word
+  end
+
+  def test_it_can_assign_point_values
+    word = Scrabble.new.asssign_point_values("HELLO")
+    assert_equal word, 8
+  end
+
+  end

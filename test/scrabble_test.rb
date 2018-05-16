93b371fd262f09dba3c1323e1b8efd477791f565
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-05-16 09:41:52 -0600

    I wrote some of my own tests, I'm very proud

--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -5,8 +5,17 @@ require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
+  def test_it_exists
+    scrabble = Scrabble.new
+    assert_instance_of Scrabble, scrabble
+  end
+
+  def test_it_has_letters
+    scrabble = Scrabble.new
+    assert "a", scrabble.point_values
+  end
   def test_it_can_score_a_single_letter
-    skip
+    # skip
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end

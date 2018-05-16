95732358982c2cf3ee8f63ff09e92be5f893ba27
JP Lynch <JPLynch35@gmail.com>
2018-05-16 09:29:15 -0600

    Add test for entire word scoring

--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -5,9 +5,26 @@ require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
-  def test_it_can_score_a_single_letter
-    skip
-    assert_equal 1, Scrabble.new.score("a")
-    assert_equal 4, Scrabble.new.score("f")
+  def test_it_exists
+    scrabble = Scrabble.new
+
+    assert_instance_of Scrabble, scrabble
   end
+
+  def test_it_can_score_a_single_letter
+    scrabble = Scrabble.new
+
+    assert_equal 1, scrabble.score("a")
+    assert_equal 4, scrabble.score("f")
+  end
+
+  def test_it_can_score_a_word
+    scrabble = Scrabble.new
+
+    assert_equal 8, scrabble.score("hello")
+    assert_equal 11, scrabble.score("school")
+  end
+
+
+
 end

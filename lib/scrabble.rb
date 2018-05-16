42f6ad3324b94b8d3f20c1fba67df65221880c06
JP Lynch <JPLynch35@gmail.com>
2018-05-16 10:50:10 -0600

    Add extension

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -2,15 +2,6 @@ require 'pry'
 
 class Scrabble
 
-  def score(word)
-    letter_array = word.upcase.chars
-    score_array = letter_array.map do |letter|
-      point_values[letter]
-    end
-    return score_array.sum
-  end
-
-
   def point_values
     {
       "A"=>1, "B"=>3, "C"=>3, "D"=>2,
@@ -23,8 +14,73 @@ class Scrabble
     }
   end
 
-  def score_with_multipliers(word, letter_multiplier, word_multiplier)
-
+  def score(word)
+    if word == "" || word == "nil"
+      @score_array = [0]
+    else
+      letter_array = word.upcase.chars
+      @score_array = letter_array.map do |letter|
+        point_values[letter]
+      end
+    end
+    return @score_array.sum
   end
+
+  def score_with_multipliers(word, letter_multiplier, word_multiplier = 1)
+    score(word)
+    post_multi_score = @score_array.map.with_index do |score, i|
+      score * letter_multiplier[i]
+    end.sum
+    final_score = check_for_letter_count_bonus(post_multi_score)
+    return final_score * word_multiplier
+  end
+
+  def check_for_letter_count_bonus(post_multi_score)
+    if @score_array.length >= 7
+      post_multi_score += 10
+    end
+    return post_multi_score
+  end
+
+  def highest_scoring_word(word_array)
+    words_and_values = create_hash_of_words_and_values(word_array)
+    max_word = words_and_values.max_by do |word, score|
+      score
+    end
+    return max_word[0]
+  end
+
+  def create_hash_of_words_and_values(word_array)
+    word_score_array = word_array.map do |word|
+      score(word)
+    end
+    Hash[word_array.zip word_score_array]
+  end
+
+
+
+
+  # def highest_scoring_word(word_array)
+  #   score_array = word_array.map do |word|
+  #     score(word)
+  #   end
+  #   if detect_more_than_one_highest_score(score_array)
+  #     word_array[score_array.index(detect_more_than_one_highest_score)]
+  #   else
+  #     find_word_with_fewest_letters(word_array, score_array)
+  #     word_array[score_array.max.index]
+  #   binding.pry
+  # end
+  #
+  # def detect_more_than_one_highest_score(score_array)
+  #   score_array.one? do |score|
+  #     score_array.max
+  #   end
+  # end
+  #
+  # def find_word_with_fewest_letters(word_array, score_array)
+  #
+  #
+  # end
 
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -6,25 +6,65 @@ require 'pry'
 
 class ScrabbleTest < Minitest::Test
   def test_it_exists
-    scrabble = Scrabble.new
+    game = Scrabble.new
 
-    assert_instance_of Scrabble, scrabble
+    assert_instance_of Scrabble, game
   end
 
   def test_it_can_score_a_single_letter
-    scrabble = Scrabble.new
+    game = Scrabble.new
 
-    assert_equal 1, scrabble.score("a")
-    assert_equal 4, scrabble.score("f")
+    assert_equal 1, game.score("a")
+    assert_equal 4, game.score("f")
   end
 
   def test_it_can_score_a_word
-    scrabble = Scrabble.new
+    game = Scrabble.new
 
-    assert_equal 8, scrabble.score("hello")
-    assert_equal 11, scrabble.score("school")
+    assert_equal 8, game.score("hello")
+    assert_equal 11, game.score("school")
   end
 
+  def test_it_will_score_nothing_as_zero
+    game = Scrabble.new
 
+    assert_equal 0, game.score("")
+    assert_equal 0, game.score("nil")
+  end
+
+  def test_score_with_l_multipliers
+    game = Scrabble.new
+    x1 = game.score_with_multipliers('hello', [1,2,1,1,1])
+
+    assert_equal 9, x1
+  end
+
+  def test_score_with_l_and_w_multipliers
+    game = Scrabble.new
+    x1 = game.score_with_multipliers('hello', [1,2,1,1,1], 2)
+
+    assert_equal 18, x1
+  end
+
+  def test_score_with_l_and_w_multipliers_and_bonus
+    game = Scrabble.new
+    x1 = game.score_with_multipliers('sparkle', [1,2,1,3,1,2,1], 2)
+
+    assert_equal 58, x1
+  end
+
+  def test_highest_scoring_word
+    game = Scrabble.new
+    x1 = game.highest_scoring_word(['home', 'word', 'hello', 'sound'])
+
+    assert_equal "home", x1
+  end
+
+  def test_highest_scoring_word_minimize_tiles
+    game = Scrabble.new
+    x1 = game.highest_scoring_word(['hello', 'word', 'sound'])
+
+    assert_equal "word", x1
+  end
 
 end

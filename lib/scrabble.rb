b60f158fc631bea351ba0a604ff05ae5e5636a1a
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-05-16 09:43:08 -0600

    refactored my changes a bit.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -6,7 +6,7 @@ class Scrabble
   end
 
   def point_values
-    letters = {
+     {
       'A' => 1, 'B' => 3, 'C' => 3, 'D' => 2,
       'E' => 1, 'F' => 4, 'G' => 2, 'H' => 4,
       'I' => 1, 'J' => 8, 'K' => 5, 'L' => 1,

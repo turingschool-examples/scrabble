72171d8d8116f10dfe244a7e4298194c37a660f2
Seth Schwartz <sschwartz214@gmail.com>
2018-05-16 10:50:44 -0600

    completes interaction pattern

--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -5,9 +5,48 @@ require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
+
   def test_it_can_score_a_single_letter
-    skip
-    assert_equal 1, Scrabble.new.score("a")
-    assert_equal 4, Scrabble.new.score("f")
+    game = Scrabble.new
+
+    assert_equal 1, game.score("a")
+    assert_equal 4, game.score("f")
+  end
+
+  def test_it_can_score_a_word
+    game = Scrabble.new
+
+    assert_equal 8, game.score("hello")
+  end
+
+  def test_it_can_score_an_empty_string
+    game = Scrabble.new
+
+    assert_equal 0, game.score("")
+  end
+
+  def test_it_can_score_nil
+    game = Scrabble.new
+
+    assert_equal 0, game.score(nil)
+  end
+
+  def test_score_a_word_with_letter_multipliers
+    game = Scrabble.new
+
+    assert_equal 9, game.score_with_multipliers('hello', [1,2,1,1,1])
+  end
+
+  def test_it_can_score_word_with_a_word_multiplier
+    game = Scrabble.new
+
+    assert_equal 18, game.score_with_multipliers('hello', [1,2,1,1,1], 2)
+  end
+
+
+  def test_it_can_score_a_word_with_a_seven_letter_bonus
+    game = Scrabble.new
+    
+    assert_equal 58, game.score_with_multipliers('sparkle', [1,2,1,3,1,2,1], 2)
   end
 end

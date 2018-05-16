aee171ff730de16897736688a6690b180ca4c6a5
Connor <connorosburn@gmail.com>
2018-05-16 10:11:11 -0600

    Got everything working

--- test/game_reader_test.rb
+++ test/game_reader_test.rb
@@ -5,5 +5,12 @@ require 'minitest/pride'
 require 'pry'
 
 class GameReaderTest < Minitest::Test
+  def test_player_data
+    game = GameReader.new('./test/input.csv')
 
+    assert_equal 3, game.word_count(:player_1)
+    assert_equal 2, game.word_count(:player_2)
+    assert_equal 23, game.score(:player_1)
+    assert_equal 18, game.score(:player_2)
+  end 
 end

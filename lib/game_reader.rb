4f865426d754c4075a4ea90249da7b6d900bfdc6
Steve Thompson <scthomps312@gmail.com>
2018-06-22 13:31:26 -0600

    Completed game_reader functionality and test

--- lib/game_reader.rb
+++ lib/game_reader.rb
@@ -1,3 +1,51 @@
 class GameReader
+  def initialize(file_name)
+    @players_and_words = {} # :player_id -> [words]
 
+    @scrabble_game = Scrabble.new
+
+    lines = File.readlines(file_name)
+
+    if(lines.length > 0)
+      first_line = lines.shift
+      prefix_word = first_line.split(',')
+      # substring using 0 .. index of _ inclusive
+      player_prefix = prefix_word[0][0, prefix_word[0].index('_') + 1]
+      lines.each do |line|
+        id_word = line.split(',')
+        id = player_prefix + id_word[0].to_s
+        id = id.to_sym
+        words = get_player_words(id)
+        if(!words)
+          words = []
+          @players_and_words[id] = words
+        end
+        words.push(id_word[1].chomp)
+      end
+    end
+  end
+
+  def word_count(player)
+    words = get_player_words(player)
+    if(words)
+      return words.length
+    end
+    0
+  end
+
+  def get_player_words(player)
+    @players_and_words[player]
+  end
+
+  def score(player)
+    words = get_player_words(player)
+    if(words)
+      total = words.reduce(0) do |score, word|
+        word_score = @scrabble_game.score(word)
+        score + word_score
+      end
+      return total
+    end
+    0
+  end
 end
--- test/game_reader_test.rb
+++ test/game_reader_test.rb
@@ -5,5 +5,17 @@ require 'minitest/pride'
 require 'pry'
 
 class GameReaderTest < Minitest::Test
+  def test_game_reader_exists
+    game_reader = GameReader.new('./test/input.csv')
+    assert_instance_of GameReader, game_reader
+  end
 
+  def test_game_reader_returns_word_counts_and_scores_for_player_ids
+    game_reader = GameReader.new('./test/input.csv')
+
+    assert_equal 3, game_reader.word_count(:player_1)
+    assert_equal 2, game_reader.word_count(:player_2)
+    assert_equal 23, game_reader.score(:player_1)
+    assert_equal 18, game_reader.score(:player_2)
+  end
 end

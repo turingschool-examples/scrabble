c10e1ec7bd3b25d25553a0ef568baeb9f653cc0b
Anten Skrabec <cebarks@gmail.com>
2018-10-10 09:24:36 -0600

    Initial Commit of scrabble.rb
    implements score method

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,13 +1,24 @@
 class Scrabble
   def point_values
     {
-      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
-      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
+      'A' => 1, 'B' => 3, 'C' => 3, 'D' => 2,
+      'E' => 1, 'F' => 4, 'G' => 2, 'H' => 4,
+      'I' => 1, 'J' => 8, 'K' => 5, 'L' => 1,
+      'M' => 3, 'N' => 1, 'O' => 1, 'P' => 3,
+      'Q' => 10, 'R' => 1, 'S' => 1, 'T' => 1,
+      'U' => 1, 'V' => 4, 'W' => 4, 'X' => 8,
+      'Y' => 4, 'Z' => 10
     }
+  end
+
+  def score(word)
+    return 0 if word.nil? || word == ''
+
+    score = 0
+    word.split('').each do |x|
+      # puts "#{x}:#{point_values[x]}"
+      score += point_values[x.upcase]
+    end
+    score
   end
 end

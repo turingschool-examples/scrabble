7d8b222638594dee49563323683e1645d3364301
jpclark6 <jpclark6@gmail.com>
2018-10-10 10:14:17 -0600

    Adds read file and count words per person

--- lib/game_reader.rb
+++ lib/game_reader.rb
@@ -1,3 +1,32 @@
-class GameReader
+require 'csv'
 
+class GameReader
+  def initialize(file)
+    all_lines = CSV.read(file)
+    all_lines.shift
+    @words_by_player = all_lines
+    # all_lines.each do |line|
+    #   @words_by_player << line.split(",")
+    # end
+  end
+
+  def word_count(player)
+    words = { player_1: 0, player_2: 0 }
+    @words_by_player.each do |line|
+      if line[0] == "1"
+        words[:player_1] += 1
+      else
+        words[:player_2] += 1
+      end
+    end
+    words[player]
+  end
 end
+
+
+# player_id,word
+# 1,hello
+# 2,hi
+# 1,silence
+# 2,snacker
+# 1,fun
--- test/game_reader_test.rb
+++ test/game_reader_test.rb
@@ -4,5 +4,15 @@ require 'minitest/pride'
 require 'pry'
 
 class GameReaderTest < Minitest::Test
+  def setup
+    @game = GameReader.new('./test/input.csv')
+  end
+  def test_it_can_create_game_readers
+    assert_instance_of GameReader, @game
+  end
 
+  def test_it_can_count_words_per_person
+    assert_equal 3, @game.word_count(:player_1)
+    assert_equal 2, @game.word_count(:player_2)
+  end
 end

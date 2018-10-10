c1848b9e022b58c60160a5bf68e981d934d7f7fd
Tony Robertson <tonyr729@gmail.com>
2018-10-10 09:39:16 -0600

    Add tests for Scrabble challenge

--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -5,8 +5,24 @@ require 'pry'
 
 class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
-    skip
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end
+
+  def test_it_can_score_an_empty_string
+    assert_equal 0, Scrabble.new.score("")
+  end
+  
+  def test_it_can_score_nil
+    assert_equal 0, Scrabble.new.score(nil)
+  end
+    
+  def test_it_can_score_with_multipliers
+    assert_equal 9, Scrabble.new.score('hello', [1,2,1,1,1])
+  end
+  
+  def test_it_can_score_with_doubler
+    assert_equal 18, Scrabble.new.score('hello', [1,2,1,1,1], 2)
+  end
+  
 end

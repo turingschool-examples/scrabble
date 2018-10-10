49e5b4b9527ee8b126ccca96e9ff8f0e6840d894
Lance Taylor <lance.taylor98@hotmail.com>
2018-10-10 09:37:41 -0600

    Getting scores to work

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,13 +1,46 @@
 class Scrabble
-  def point_values
+  def initialize
+    @point_values =
     {
-      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
-      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
+      "A" => 1,
+      "B" => 3,
+      "C" => 3,
+      "D" => 2,
+      "E" => 1,
+      "F" => 4,
+      "G" => 2,
+      "H" => 4,
+      "I" => 1,
+      "J" => 8,
+      "K" => 5,
+      "L" => 1,
+      "M" => 3,
+      "N" => 1,
+      "O" => 1,
+      "P" => 3,
+      "Q" => 10,
+      "R" => 1,
+      "S" => 1,
+      "T" => 1,
+      "U" => 1,
+      "V" => 4,
+      "W" => 4,
+      "X" => 8,
+      "Y" => 4,
+      "Z" => 10
     }
+  end
+
+  def score(word)
+    if word.length == 1
+      @point_values[word]
+
+    elsif new_word = word.upcase.split(//)
+    scored_word = new_word.map do |letter|
+      @point_values[letter]
+    end
+    p scored_word
+    end
+
   end
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -5,8 +5,10 @@ require 'pry'
 
 class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
-    skip
+    Scrabble.new.score("a")
+    Scrabble.new.score("f")
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end
+  binding.pry
 end

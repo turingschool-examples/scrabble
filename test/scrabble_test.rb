3aaeca51cf9d2f4a0e4596b7cc19014fd6600046
jpclark6 <jpclark6@gmail.com>
2018-10-10 09:32:35 -0600

    Adds scoring capability

--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -4,9 +4,29 @@ require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
+  def setup
+    @scrabble = Scrabble.new
+  end
+
   def test_it_can_score_a_single_letter
-    skip
-    assert_equal 1, Scrabble.new.score("a")
-    assert_equal 4, Scrabble.new.score("f")
+    assert_equal 1, @scrabble.score("a")
+    assert_equal 4, @scrabble.score("f")
+    assert_equal 0, @scrabble.score("")
+    assert_equal 0, @scrabble.score(nil)
+  end
+
+  def test_it_can_score_words
+    assert_equal 5, @scrabble.score("APE")
+    assert_equal 12, @scrabble.score("zoo")
+  end
+
+  def test_it_can_take_multipliers
+    assert_equal 6, @scrabble.score_with_multipliers("ape", [2, 1, 1])
+    assert_equal 12, @scrabble.score_with_multipliers("ape", [2, 1, 1], 2)
+  end
+
+  def test_it_can_do_seven_letter_bonus
+    assert_equal 23, @scrabble.score_with_multipliers("mountain", [1, 2, 3, 1, 1, 1, 1, 1])
+    assert_equal 46, @scrabble.score_with_multipliers("mountain", [1, 2, 3, 1, 1, 1, 1, 1], 2)
   end
 end

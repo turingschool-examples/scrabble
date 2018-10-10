6b1cbd95a384f240c906deeb78d8bef354f3ed5d
abenetka <ali.benetka@gmail.com>
2018-10-10 09:39:25 -0600

    Adds test for existence

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,6 +1,6 @@
 class Scrabble
-  def point_values
-    {
+  def initialize
+    @point_values = {
       "A"=>1, "B"=>3, "C"=>3, "D"=>2,
       "E"=>1, "F"=>4, "G"=>2, "H"=>4,
       "I"=>1, "J"=>8, "K"=>5, "L"=>1,
@@ -9,5 +9,15 @@ class Scrabble
       "U"=>1, "V"=>4, "W"=>4, "X"=>8,
       "Y"=>4, "Z"=>10
     }
+  end
+
+  def score(point_values)
+    point_values.upcase
+    points_value.to_i
+    point_values.map do |letter|
+      @point_values[letter]
+    end
+
+
   end
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -4,9 +4,17 @@ require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
+  def test_it_exits
+    game = Scrabble.new
+    assert_instance_of Scrabble, game
+  end
+
   def test_it_can_score_a_single_letter
-    skip
+    # skip
+    game = Scrabble.new
+    # binding.pry
     assert_equal 1, Scrabble.new.score("a")
-    assert_equal 4, Scrabble.new.score("f")
+    # assert_equal 4, Scrabble.new.score("f")
+    # binding.pry
   end
 end

c6ac2f7198df8d120b5fefac7e7cf3744b3f2934
Tony Robertson <tonyr729@gmail.com>
2018-10-10 10:09:50 -0600

    Add outline for CSV parse test

--- test/game_reader_test.rb
+++ test/game_reader_test.rb
@@ -4,8 +4,18 @@ require 'minitest/pride'
 require 'pry'
 
 class GameReaderTest < Minitest::Test
+  def setup
+    @game = GameReader.new('./test/input.csv')
+  end
+
   def test_it_exists
-    game = GameReader.new('./test/input.csv')
-    assert_instance_of GameReader, game
+    assert_instance_of GameReader, @game
+  end
+
+  def test_it_can_parse_csv
+    parsed_obj = @game.parse_csv
+    expected = {}
+    
+    assert_equal expected, parsed_obj
   end
 end

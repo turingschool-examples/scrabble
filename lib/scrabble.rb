5c8348c2c77df43334b84bb67e90d6bdf5fe9ca9
Aizar <aizar.cabrera@icloud.com>
2018-10-10 09:15:45 -0600

    add assertion for existence

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,6 +1,6 @@
 class Scrabble
   def point_values
-    {
+    alphabet = {
       "A"=>1, "B"=>3, "C"=>3, "D"=>2,
       "E"=>1, "F"=>4, "G"=>2, "H"=>4,
       "I"=>1, "J"=>8, "K"=>5, "L"=>1,
@@ -9,5 +9,8 @@ class Scrabble
       "U"=>1, "V"=>4, "W"=>4, "X"=>8,
       "Y"=>4, "Z"=>10
     }
+  end
+
+  def score(letter)
   end
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -4,9 +4,19 @@ require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
-  def test_it_can_score_a_single_letter
-    skip
-    assert_equal 1, Scrabble.new.score("a")
-    assert_equal 4, Scrabble.new.score("f")
+
+  def setup
+    @scrabble = Scrabble.new
   end
+
+  def test_it_exists
+    assert_instance_of Scrabble, @scrabble
+  end
+
+  def test_it_can_score_a_single_letter
+    # assert_equal 1, @scrabble.score("a")
+    # assert_equal 4, @scrabble.score("f")
+  end
+
+
 end

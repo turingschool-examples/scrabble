4d4f0668936e7d2d759583d767e1e2e86da18d07
Mary Goodhart <marya.grace5@gmail.com>
2018-10-10 10:16:53 -0600

    Has not made another passing test or finished iteration 1

--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -7,7 +7,9 @@ class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
-    require 'pry';binding.pry
+  end
 
+  def test_it_can_score_a_word
+    assert_equal 8, Scrabble.new.score("hello")
   end
 end

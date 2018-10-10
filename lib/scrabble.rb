c54b367cdfd8378d924303f99688dcd7109e7f4f
Justin Mauldin <justin.mauldin7@gmail.com>
2018-10-10 09:58:55 -0600

    Adds attempt 2 at getting score to work.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,13 +1,14 @@
 class Scrabble
-  def point_values
+  def initialize
+    @point_values =
     {
-      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
-      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
+      "A" => 1, "B" => 3, "C" => 3, "D" => 2,
+      "E" => 1, "F" => 4, "G" => 2, "H" => 4,
+      "I" => 1, "J" => 8, "K" => 5, "L" => 1,
+      "M" => 3, "N" => 1, "O" => 1, "P" => 3,
+      "Q" => 10, "R" => 1, "S" => 1, "T" => 1,
+      "U" => 1, "V" => 4, "W" => 4, "X" => 8,
+      "Y" => 4, "Z" => 10
     }
   end
 
@@ -17,8 +18,7 @@ class Scrabble
     get_letters = @player_input.split('')
 
     score = get_letters.map do |find_value|
-      binding.pry 
-      point_values[find_value]
+      @point_values[find_value]
     end
     score.join('')
   end

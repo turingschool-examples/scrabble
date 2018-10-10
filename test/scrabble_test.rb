8979764ddeccdd2252e7c55535a755c5e4b01b15
vjp888 <rellik888@gmail.com>
2018-10-10 10:57:25 -0600

    Added checks for empty string and nil began psuedocode for highest word score

--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -7,6 +7,8 @@ class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
+    assert_equal 0, Scrabble.new.score("")
+    assert_equal 0, Scrabble.new.score(nil)
   end
 
   def test_it_can_handle_words_varing_caps
@@ -28,3 +30,27 @@ class ScrabbleTest < Minitest::Test
     assert_equal 8, short
   end
 end
+
+
+#Psuedo coding highest scoring word
+
+#method takes in 3 seperate strings in array
+#feed strings into conversions to get values and compare
+#Highest value wins - print out that word
+
+#Restraints - If there is a tie the shortest word wins
+#If a word is seven letters long it automatically wins (+10 bonus)
+
+#If the words are the same length and score pick the first one in the array.
+
+# def highest_scoring_word(array)
+#   get their values, include the bonus for 7+ (maybe zip into orig array?)
+#   compare values
+#   return the winner
+#   if values are tied determine winner by shortest length val1 > val2
+#     return winner
+#   if word is 7 chars
+#       return word
+#
+#   if words are same length and value go up array and pick first in list
+#   end

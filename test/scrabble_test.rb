bf55d6e8811db8ae4af2ab5e4b7bc8e48ea87095
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-10 10:12:38 -0600

    Adds test for scoring single letters and a word

--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -4,9 +4,16 @@ require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
+  def setup
+    @scrabble = Scrabble.new
+  end
+
   def test_it_can_score_a_single_letter
-    skip
-    assert_equal 1, Scrabble.new.score("a")
-    assert_equal 4, Scrabble.new.score("f")
+    assert_equal 1, @scrabble.score("a")
+    assert_equal 4, @scrabble.score("f")
+  end
+
+  def test_it_can_score_a_word_with_no_multipliers
+    assert_equal 8, @scrabble.score("hello")
   end
 end

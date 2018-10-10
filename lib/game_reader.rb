942a92fa7e5b0548019256c7a912f8f8e2f9f855
Tony Robertson <tonyr729@gmail.com>
2018-10-10 10:31:16 -0600

    Add code to try and translate CSV into game.
    Attempt to call method of Scrabble to add play for each row of CSV

--- lib/game_reader.rb
+++ lib/game_reader.rb
@@ -1,5 +1,21 @@
+require 'CSV'
+require_relative './scrabble'
+require 'pry'
+
 class GameReader
-  def initialize(csv)
-    @csv = csv
+  def initialize(csv_path)
+    @csv_path = csv_path
+    @scrabble = Scrabble.new
+  end
+
+  def parse_csv
+    record = {}
+    CSV.foreach(@csv_path) do |row|
+      if row[0] != "player_id"
+        player_sym = "player_#{row[0]}".to_sym
+        @scrabble.add_play(player_sym, row[1])
+      end
+    end
+    binding.pry
   end
 end
